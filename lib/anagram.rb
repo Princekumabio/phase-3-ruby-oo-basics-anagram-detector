6b88cfb875fd4b804cbda2edfa6c50300cdd177a
kumabio <kumabio.prince@student.moringaschool.com>
2022-09-01 10:03:27 +0000

    Completed Assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word        
+    end
+
+    def match(arrays)
+        new_array = arrays.filter do |ab|
+            @word.chars.sort == ab.chars.sort
+         end
+        new_array
+    end
+end
